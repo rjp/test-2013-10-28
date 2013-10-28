bda141b4c2a86419645fac18c6b2997936feb6cd
rjp <rjpartington@googlemail.com>
2013-10-28 18:55:39 +0000

    Keep an orderable version of the card's value handy
    Since we're ranking and ranking is ordering, it makes sense to keep an
    orderable version of the card's value easily accessible - you could make
    this a method which calculated it each time but since it's static, we
    might as well do it on construction to save effort later.

--- pokercard.rb
+++ pokercard.rb
@@ -1,9 +1,12 @@
+
 class PokerCard
-    attr_accessor :suit, :value
+    attr_accessor :suit, :value, :int_value
+    @@int_values = { 'T' => 10, 'J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14 }
 
     def initialize(value, suit)
         @value = value
         @suit = suit
+        @int_value = @@int_values[value] || value.to_i
     end
 
     def PokerCard.new_from_string(card_string)
