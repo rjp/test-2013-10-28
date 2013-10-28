bd2948db54fa1064b39a1d64762d098c3db81608
rjp <rjpartington@googlemail.com>
2013-10-28 19:19:58 +0000

    Final on-the-fly ranking code
    This feels like terrible cheating and I'm sure there's a cleverer way
    to do this - representing a hand as a collection of 5 6-bit values
    (2 bits for hand, 4 bits for card) with the appropriate bitmasking and
    comparisons might work.  Haven't really thought that through.  On the
    other hand, this is a 2 hour project and getting something running is
    vastly more important than making a lovely crystal space castle.
    
    I hope.

--- pokerhand.rb
+++ pokerhand.rb
@@ -8,11 +8,11 @@ class PokerHand
         @cards = cards
 
         @by_suit = {}
-        @by_value = {}
+        @by_value = Hash.new(0)
 
         @cards.each do |c|
             @by_suit[c.suit] = c.suit
-            @by_value[c.value] = c.value
+            @by_value[c.value] = @by_value[c.value] + 1
         end
 
         # Because we're lazy, keep a sorted copy of the hand around
@@ -25,15 +25,45 @@ class PokerHand
     end
 
     def rank
-        # One suit => Flush or Straight Flush
-        if @by_suit.keys.size == 1 then
-            # If the numerical value of last-first is 4, we have a sequence
-            # running from X to X+4 => Straight Flush => JACKPOT
-            if @sorted_cards[-1].int_value - @sorted_cards[0].int_value == 4 then
+        # Distinguish Straight Flush from Straight
+        if @sorted_cards[-1].int_value - @sorted_cards[0].int_value == 4 then
+            # We have 5 cards in order
+            if @by_suit.keys.size == 1 then
                 return 'Straight Flush'
             else
-                return 'Flush'
+                return 'Straight'
             end
+        end
+
+        # 5 cards, 1 suit, not ordered => Flush
+        if @by_suit.keys.size == 1 then
+            return 'Flush'
+        end
+
+        s_values = @by_value.sort_by { |i| -i[1] }
+        p s_values
+
+        # If we only have two values, it's Full House or 4OAK
+        if s_values.size == 2 then
+            if s_values[0][1] == 4 then
+                return 'Four Of A Kind'
+            else
+                return 'Full House'
+            end
+        end
+
+        # Three values => Two Pairs or 3OAK
+        if s_values.size == 3 then
+            if s_values[0][1] == 3 then
+                return 'Three Of A Kind'
+            else
+                return 'Two Pair'
+            end
+        end
+
+        # Maybe we have a pair
+        if s_values[0][1] == 2 then
+            return 'Pair'
         end
 
         return 'High Card'
