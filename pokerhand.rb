e6b0b34eb7384284cfb1b9250646fa7700df83d2
rjp <rjpartington@googlemail.com>
2013-10-28 18:50:00 +0000

    More faux-flesh on the PokerHand bones
    Add hard-coded `rank` and `value` methods for test fudging.  Of course,
    now they're not `attr_accessor` any more; remove that.

--- pokerhand.rb
+++ pokerhand.rb
@@ -1,7 +1,7 @@
 require 'pokercard'
 
 class PokerHand
-    attr_accessor :rank, :value, :cards
+    attr_accessor :cards
 
     def initialize(cards)
         @cards = cards
@@ -10,5 +10,13 @@ class PokerHand
     def PokerHand.new_from_string(card_string)
         card_names = card_string.split(' ')
         return PokerHand.new( card_names.map { |cs| PokerCard.new_from_string(cs) } )
+    end
+
+    def rank
+        return 'High Card'
+    end
+
+    def value
+        return ['Q']
     end
 end
