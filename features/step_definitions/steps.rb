c02c4d62258d14ab616a76dbee5c792cf01fb3bc
rjp <rjpartington@googlemail.com>
2013-10-28 18:42:06 +0000

    Test whether the hand has 5 cards
    This should always pass but it's a simple way of checking whether tests
    are being run and constructing objects appropriately.

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -3,6 +3,7 @@ require 'pokerhand'
 
 Given /^I have "(.+)"/ do |cards|
     @hand = PokerHand.new_from_string(cards)
+    @hand.cards.size.should eq(5)
 end
 
 Then /^my ranking is "(.+?)" with value (.+)/ do |rank, value|
