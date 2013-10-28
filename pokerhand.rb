c8f33983c9533f1c56febb7ef15e8f268ddaae30
rjp <rjpartington@googlemail.com>
2013-10-28 19:03:04 +0000

    Starting to rank hands by Poker rules
    (ab)Use our sorted list of cards and "count of suits" for Flushes.
    
    Q. Since we have a static hand, would it not be better to put this
    into the constructor rather than calculating it later?  Do we gain
    anything by having this "on the fly"?  (A: If we have a river/flop,
    probably, since then cards not in the hand can influence the rank,
    maybe.  If I remember correctly.  Which I frequently don't with Poker.)

--- pokerhand.rb
+++ pokerhand.rb
@@ -25,10 +25,21 @@ class PokerHand
     end
 
     def rank
+        # One suit => Flush or Straight Flush
+        if @by_suit.keys.size == 1 then
+            # If the numerical value of last-first is 4, we have a sequence
+            # running from X to X+4 => Straight Flush => JACKPOT
+            if @sorted_cards[-1].int_value - @sorted_cards[0].int_value == 4 then
+                return 'Straight Flush'
+            else
+                return 'Flush'
+            end
+        end
+
         return 'High Card'
     end
 
     def value
-        return ['Q']
+        return @sorted_cards.reverse.map { |c| c.value }
     end
 end
