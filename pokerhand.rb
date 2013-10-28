0ef03cfd94082b2a9af85f5faf387f72fcedbb3c
rjp <rjpartington@googlemail.com>
2013-10-28 18:39:50 +0000

    Flesh out the PokerHand class slightly
    Now we can create a hand from a string - this is a class method which
    returns a PokerHand object rather than creating an object and then
    filling it with cards.  Slightly nicer in my head.

--- pokerhand.rb
+++ pokerhand.rb
@@ -1,3 +1,14 @@
+require 'pokercard'
+
 class PokerHand
-    attr_accessor :rank, :value
+    attr_accessor :rank, :value, :cards
+
+    def initialize(cards)
+        @cards = cards
+    end
+
+    def PokerHand.new_from_string(card_string)
+        card_names = card_string.split(' ')
+        return PokerHand.new( card_names.map { |cs| PokerCard.new_from_string(cs) } )
+    end
 end
