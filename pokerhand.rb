d284db8238e50c359a32224d07d95f7a39743672
rjp <rjpartington@googlemail.com>
2013-10-28 18:57:09 +0000

    More flesh for PokerHand
    Keep a numerically sorted list of the cards we have since it simplifies
    life later.  Also keep a hash of the suits and values we have.

--- pokerhand.rb
+++ pokerhand.rb
@@ -1,10 +1,22 @@
 require 'pokercard'
 
+
 class PokerHand
-    attr_accessor :cards
+    attr_accessor :cards, :sorted_cards, :by_suit, :by_value
 
     def initialize(cards)
         @cards = cards
+
+        @by_suit = {}
+        @by_value = {}
+
+        @cards.each do |c|
+            @by_suit[c.suit] = c.suit
+            @by_value[c.value] = c.value
+        end
+
+        # Because we're lazy, keep a sorted copy of the hand around
+        @sorted_cards = @cards.sort_by { |i| i.int_value }
     end
 
     def PokerHand.new_from_string(card_string)
